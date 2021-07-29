a5235a81cc3681ba812a8df87dc95e277d100f84
“Casso” <1152283574@qq.com>
2021-07-29 10:34:37 +0800

    fead(app/service):http server

--- app/user/service/internal/server/grpc.go
+++ app/user/service/internal/server/grpc.go
@@ -27,8 +27,8 @@ func NewGRPCServer(c *conf.Server, logger log.Logger, tp *tracesdk.TracerProvide
 	if c.Grpc.Network != "" {
 		opts = append(opts, grpc.Network(c.Grpc.Network))
 	}
-	if c.Grpc.Network != "" {
-		opts = append(opts, grpc.Address(c.Grpc.Network))
+	if c.Grpc.Addr != "" {
+		opts = append(opts, grpc.Address(c.Grpc.Addr))
 	}
 	if c.Grpc.Timeout != nil {
 		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
