6d604cc1890f53d0bb97eee08f8d70e8be737442
casso <1152283574@qq.com>
2021-11-24 18:35:35 +0800

    faed(pkg/util/str):string utils

---
+++ pkg/util/str/str.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: Casso
+ * @Date: 2021-11-23 17:49:03
+ * @LastEditors: Casso
+ * @LastEditTime: 2021-11-23 17:50:35
+ * @Description: 字符串处理
+ * @FilePath: /kratos-mono-repo/pkg/util/str/str.go
+ */
+package str
