19440b15d245da548bbd2ec6b32866cdcfd767b5
casso <1152283574@qq.com>
2021-12-23 14:32:34 +0800

    test(pkg/util/bigint):fix testing

--- pkg/util/bigint/bigint_test.go
+++ pkg/util/bigint/bigint_test.go
@@ -4,11 +4,11 @@ import "testing"
 
 func TestParserPrice(t *testing.T) {
 	source := map[float64]string{
-		6.3: "6300000000000000000",
+		6.3: "63000000000000000001",
 	}
 	for k, v := range source {
 		if ParserPrice(k) != v {
-			t.Errorf("want: 6000000000000000000,got %s", ParserPrice(k))
+			t.Errorf("want: %s,got %s", v, ParserPrice(k))
 		}
 	}
 }
