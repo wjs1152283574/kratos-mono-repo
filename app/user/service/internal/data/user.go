776f637b849e504fbafef4a0c5e59f4f5b6d3af8
Casso <1152283574@qq.com>
2022-01-30 20:00:45 +0800

    fix(app/user/internal/data):remove tips

--- app/user/service/internal/data/user.go
+++ app/user/service/internal/data/user.go
@@ -1,3 +1,11 @@
+/*
+ * @PackageName:
+ * @Description:
+ * @Author: Casso
+ * @Date: 2022-01-29 14:58:02
+ * @LastModifiedBy: Casso
+ * @LastEditTime: 2022-01-30 19:59:57
+ */
 package data
 
 import (
@@ -74,7 +82,6 @@ func (r *UserRepo) List(ctx context.Context, pageNum, pageSize int64) ([]*biz.Us
 	return userList, nil
 }
 
-// GetToken check user exit and return token
 func (r *UserRepo) GetUserByMobile(ctx context.Context, mobile string) (user *biz.User, err error) {
 	if err = r.data.db.WithContext(ctx).Where("mobile = ?", mobile).First(&user).Error; err != nil {
 		return &biz.User{}, normal.RecordNotFound
