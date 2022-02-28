993a02c40e160f8458ace8fb2fc13c792e73ffa8
“casso” <1152283574@qq.com>
2022-02-28 21:51:32 +0800

    feat(pkg/util/bigint):大数操作

--- pkg/util/bigint/bigint.go
+++ pkg/util/bigint/bigint.go
@@ -1,22 +1,37 @@
 package bigint
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
+	"strconv"
 )
 
-var sock = "1000000000000000000"
+// PrecisionPrice 转大数专用 传入 价格，精度
+func ParserPrice(price uint64, precision int64) (res string, err error) {
+	if precision == 0 {
+		res = fmt.Sprintf("%v", price)
+		return
+	}
+	i, err := strconv.ParseFloat(fmt.Sprintf("1e+%v", precision), 64)
+	if err != nil {
+		return
+	}
 
-// ParserPrice 浮点数 * 基数 返回运算后的字符串
-func ParserPrice(price float64) string {
-	var a, _ = new(big.Rat).SetString(fmt.Sprintf("%f", price))
-	var b, _ = new(big.Rat).SetString(sock)
-	a.Mul(a, b)
-	var c, _ = new(big.Rat).SetString(a.String())
-	return c.RatString()
+	res = new(big.Int).Mul(big.NewInt(int64(i)), new(big.Int).SetUint64(price)).String()
+	return
 }
 
-// PrecisionPrice 转6位大数
-func PrecisionPrice6(price uint64) string {
-	return new(big.Int).Mul(big.NewInt(1e+6), new(big.Int).SetUint64(price)).String()
+// Commisssion 获取扣除手续费后的金额  传入 总价大数，手续费（例如0.2） 返回字符串
+func Commisssion(total string, commiss float64) (string, error) {
+	var err error
+	bbint, ok := new(big.Int).SetString(total, 10)
+	if !ok {
+		return "", errors.New("bigint SetString fail")
+	}
+	f1 := big.NewFloat(float64(bbint.Int64()))
+	f3 := big.NewFloat(float64(bbint.Int64()))
+	yk := f1.Mul(f1, big.NewFloat(commiss))
+	result := f1.Sub(f3, yk)
+	return result.String(), err
 }
